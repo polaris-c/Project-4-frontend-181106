e4b84af39ae95241e04cff8c83e25604a82e0c01
e <1169846461@qq.com>
2018-11-22 22:49:25 +0800

    zsy master complete explosive-creation layout

--- src/views/sample-management/explosive-sample/explosive-creation.vue
+++ src/views/sample-management/explosive-sample/explosive-creation.vue
@@ -1,36 +1,204 @@
 <template>
   <div class="dashboard-container">
-    explosive-creation
-    <div>name:{{ name }}</div>
+    <el-row class="el-row-style">
+      <el-col :span="24">
+        <el-card class="" shadow="hover">
+          <div slot="header">
+            <span>炸药与原材料基本信息</span>
+          </div>
+          <div>
+            <el-form 
+              ref="form" 
+              :model="formData"
+              :rules="formRule"
+              :label-position="labelPosition"
+              label-width="100px">
 
-    <div>
-      <el-form 
-        ref="form" 
-        :model="formData"
-        :rules="formRule"
-        :label-position="labelPosition"
-        label-width="100px">
-        <el-form-item label="样本名称">
-          <el-input v-model="formData.sname"></el-input>
-        </el-form-item>
-        <el-form-item label="样本名称缩写">
-          <el-input v-model="formData.snameAbbr"></el-input>
-        </el-form-item>
-        <el-form-item label="样本产地">
-          <el-input v-model="formData.sampleOrigin"></el-input>
-        </el-form-item>
-        <el-form-item label="样本厂家">
-          <el-input v-model="formData.factory"></el-input>
-        </el-form-item>
-        <el-form-item label="备注">
-          <el-input 
-            v-model="formData.note"
-            type="textarea"
-            placeholder="请输入...">
-          </el-input>
-        </el-form-item>
-      </el-form>
-    </div>
+              <el-row :gutter="40">
+                <el-col :span="12">
+                  <el-form-item label="样本名称">
+                    <el-input v-model="formData.sname"></el-input>
+                  </el-form-item>
+                </el-col>
+                <el-col :span="12">
+                  <el-form-item label="样本名称缩写">
+                    <el-input v-model="formData.snameAbbr"></el-input>
+                  </el-form-item>
+                </el-col>
+              </el-row>
+
+              <el-row :gutter="40">
+                <el-col :span="12">
+                  <el-form-item label="样本产地">
+                    <el-input v-model="formData.sampleOrigin"></el-input>
+                  </el-form-item>
+                </el-col>
+                <el-col :span="12">
+                  <el-form-item label="样本厂家">
+                    <el-input v-model="formData.factory"></el-input>
+                  </el-form-item>
+                </el-col>
+              </el-row>
+
+              <el-row :gutter="40">
+                <el-col :span="12">
+                  <el-form-item label="备注">
+                    <el-input 
+                      v-model="formData.note"
+                      type="textarea"
+                      placeholder="请输入...">
+                    </el-input>
+                  </el-form-item>
+                </el-col>
+                <el-col :span="12"></el-col>
+              </el-row>
+              
+            </el-form>
+          </div>
+        </el-card>
+      </el-col>
+    </el-row>
+
+     <!-- 检测方法  -->
+    <el-row :gutter="20" class="el-row-style">
+
+      <el-col :span="8">
+        <el-card shadow="hover">
+          <div slot="header">
+            <span>FTIR</span>
+          </div>
+          <div>
+            <el-form 
+              ref="FTIR" 
+              :model="FTIRData"
+              :label-position="labelPosition"
+              label-width="100px">
+              <el-form-item label="检测设备">
+                <el-input v-model="FTIRData.devDetect"></el-input>
+              </el-form-item>
+              <el-form-item label="检测方法">
+                <el-input v-model="FTIRData.methodDetect"></el-input>
+              </el-form-item>
+              <el-form-item label="上传文件">
+                <el-upload
+                  action=""
+                  ref="uploadFTIR"
+                  :limit="10"
+                  :file-list="FTIRFile"
+                  :on-change="handleChange"
+                  :on-preview="handlePreview"
+                  :before-upload="beforeFileUpload"> 
+                  <!-- :on-change="handleChange"  :auto-upload="false"-->
+                  <el-button slot="trigger" size="small" type="primary">点击选取</el-button>
+                </el-upload>
+              </el-form-item>
+            </el-form>
+          </div>
+        </el-card>
+      </el-col>
+
+      <el-col :span="8">
+        <el-card shadow="hover">
+          <div slot="header">
+            <span>RAMAN</span>
+          </div>
+          <div>
+            <el-form 
+              ref="FTIR" 
+              :model="FTIRData"
+              :label-position="labelPosition"
+              label-width="100px">
+              <el-form-item label="检测设备">
+                <el-input v-model="FTIRData.devDetect"></el-input>
+              </el-form-item>
+              <el-form-item label="检测方法">
+                <el-input v-model="FTIRData.methodDetect"></el-input>
+              </el-form-item>
+              <el-form-item label="上传文件">
+                <el-button size="small" type="primary">点击上传</el-button>
+              </el-form-item>
+            </el-form>
+          </div>
+        </el-card>
+      </el-col>
+
+      <el-col :span="8">
+        <el-card shadow="hover">
+          <div slot="header">
+            <span>XRD</span>
+          </div>
+          <div>
+            <el-form 
+              ref="FTIR" 
+              :model="FTIRData"
+              :label-position="labelPosition"
+              label-width="100px">
+              <el-form-item label="检测设备">
+                <el-input v-model="FTIRData.devDetect"></el-input>
+              </el-form-item>
+              <el-form-item label="检测方法">
+                <el-input v-model="FTIRData.methodDetect"></el-input>
+              </el-form-item>
+              <el-form-item label="上传文件">
+                <el-button size="small" type="primary">点击上传</el-button>
+              </el-form-item>
+            </el-form>
+          </div>
+        </el-card>
+      </el-col>
+
+    </el-row>
+
+    <el-row :gutter="20" class="el-row-style">
+
+      <el-col :span="8">
+        <el-card shadow="hover">
+          <div slot="header">
+            <span>XRD</span>
+          </div>
+          <div>
+            <el-form 
+            ref="FTIR" 
+            :model="FTIRData"
+            :label-position="labelPosition"
+            label-width="100px">
+            <el-form-item label="检测设备">
+              <el-input v-model="FTIRData.devDetect"></el-input>
+            </el-form-item>
+            <el-form-item label="检测方法">
+              <el-input v-model="FTIRData.methodDetect"></el-input>
+            </el-form-item>
+          </el-form>
+          </div>
+        </el-card>
+      </el-col>
+
+      <el-col :span="8">
+        <el-card shadow="hover">
+          <div slot="header">
+            <span>GCMS</span>
+          </div>
+          <div>
+            <el-form 
+            ref="FTIR" 
+            :model="FTIRData"
+            :label-position="labelPosition"
+            label-width="100px">
+            <el-form-item label="检测设备">
+              <el-input v-model="FTIRData.devDetect"></el-input>
+            </el-form-item>
+            <el-form-item label="检测方法">
+              <el-input v-model="FTIRData.methodDetect"></el-input>
+            </el-form-item>
+          </el-form>
+          </div>
+        </el-card>
+      </el-col>
+
+      <el-col :span="8">
+      </el-col>
+
+    </el-row>
   </div>
 </template>
 
@@ -43,7 +211,6 @@ export default {
     return {
       labelPosition: 'left',
       formData: {
-          // id: '001',
           sname: '',
           snameAbbr: '',
           sampleOrigin: '',
@@ -54,8 +221,13 @@ export default {
           picURL: ''
       },
       formRule: {
-
-      }
+      },
+      FTIRData: {
+        devDetect: '',
+        methodDetect: ''
+      },
+      FTIRFile: [],
+      uploadFTIRFile: {}
     }
   },
   computed: {
@@ -69,17 +241,66 @@ export default {
     ])
   },
 
+  mounted() {
+    this.uploadFTIRFile = new FormData()
+    console.log('- - mounted - - uploadFTIRFile', this.uploadFTIRFile)
+  },
+
+  methods: {
+    handleChange(file, fileList) {
+      for(let fileItem of fileList) {
+        // console.log('- - Change - - fileItem:', fileItem)
+      }
+      // this.FTIRFile = fileList
+      // console.log('- - Change - - FTIRFile:', this.FTIRFile)
+      // console.log('- - Change - - file:', file)
+      // this.uploadFTIRFile.set('txtURL', file)
+      // console.log('- - Change - - uploadFTIRFile.txtURL:', this.uploadFTIRFile)
+    },
+    beforeFileUpload(file) {
+      console.log('- - before - - file:', file)
+
+      this.FTIRFile.push(file)
+      console.log('- - before - - FTIRFile:', this.FTIRFile)
+      
+      this.uploadFTIRFile.append('txtURL', file)
+      // console.log('- - before- - uploadFTIRFile:', this.uploadFTIRFile)
+
+      for(let key of this.uploadFTIRFile.keys()) {
+        // console.log('- - before- - uploadFTIRFile key:', key)
+      }
+      for(let value of this.uploadFTIRFile.values()) {
+        console.log('- - before- - uploadFTIRFile value:', value)
+      }
+      return false
+    },
+    handlePreview(file) {
+      console.log('- - Preview - - file:', file.name, file)
+    }
+
+  }
+
 }
 </script>
 
 <style rel="stylesheet/scss" lang="scss" scoped>
 .dashboard {
   &-container {
-    margin: 30px;
+    padding: 15px;
   }
   &-text {
     font-size: 30px;
     line-height: 46px;
   }
+}
+
+.main {
+  &-form {
+    width: 400px;
+  }
+}
+
+.el-row-style {
+  margin-bottom: 20px;
 }
 </style>
---
+++ src/views/sample-management/explosive-sample/ingredient-component.vue
@@ -0,0 +1,70 @@
+<template>
+  <el-card shadow="hover">
+
+    <div slot="header">
+      <span>FTIR</span>
+    </div>
+
+    <div>
+      <el-form 
+      ref="form" 
+      :model="formData"
+      :label-position="labelPosition"
+      label-width="100px"
+      class="main-form">
+        <el-form-item label="样本名称">
+          <el-input v-model="formData.sname"></el-input>
+        </el-form-item>
+        <el-form-item label="样本名称缩写">
+          <el-input v-model="formData.snameAbbr"></el-input>
+        </el-form-item>
+      </el-form>
+    </div>
+
+  </el-card>
+</template>
+
+<script>
+import { mapGetters } from 'vuex'
+
+export default {
+  name: 'FTIR',
+  data() {
+    return {
+    }
+  },
+  computed: {
+    ...mapGetters([
+      'name',
+      'roles',
+      'sidebar',
+      'device',
+      'token',
+      'avatar',
+    ])
+  },
+  watch: {
+    $route() {
+      console.log('- - - - detail route.params: ', this.$route.params.id)
+    }
+  },
+  mounted() {
+    console.log('- - - - detail route.params: ', this.$route.params.id)
+  },
+  methods: {
+
+  }
+}
+</script>
+
+<style rel="stylesheet/scss" lang="scss" scoped>
+.dashboard {
+  &-container {
+    margin: 30px;
+  }
+  &-text {
+    font-size: 30px;
+    line-height: 46px;
+  }
+}
+</style>
