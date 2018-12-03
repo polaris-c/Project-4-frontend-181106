33aee2f535268c54e89622a7b86dbb43dff837a3
e <1169846461@qq.com>
2018-12-03 20:26:36 +0800

    zsy master try add devPartData in deviceSamle

--- src/views/sample-management/device-sample/device-creation.vue
+++ src/views/sample-management/device-sample/device-creation.vue
@@ -78,11 +78,28 @@
     </el-row>
 
     <!-- 零件信息 -->
-    <el-row class="el-row-style">
+    <el-row 
+      class="el-row-style"
+      v-for="(devPartData, index) in devPartDataList"
+      :key="devPartData.key">
       <el-col :span="24">
         <el-card shadow="hover">
           <div slot="header">
-            <span>零件信息</span>
+            <el-row>
+              <el-col :span="23">
+                <span>零件信息   ( {{index+1}} )</span>
+              </el-col>
+              <el-col :span="1">
+                <el-button
+                  type="danger" 
+                  size="mini"
+                  icon="el-icon-close"
+                  @click="handleDeleteDevPart(index)"
+                  circle>
+                </el-button>
+              </el-col>
+            </el-row>
+            
           </div>
           <!-- 零件基本信息 -->
           <div>
@@ -175,11 +192,122 @@
  
             </el-form>
           </div>
+
           <!-- 零件特征 -->
           <div>
             <el-collapse > 
               <el-collapse-item title="成分特征" name="ingredient">
-                1
+                <el-row :gutter="20" class="el-row-style">
+
+                  <el-col :span="8">
+                    <el-card shadow="hover">
+                      <div slot="header">
+                        <span>FTIR</span>
+                      </div>
+                      <div>
+                        <el-form 
+                          ref="FTIR" 
+                          :model="devPartData.FTIRdata"
+                          :label-position="labelPosition"
+                          label-width="100px">
+                          <el-form-item label="检测设备">
+                            <el-input v-model="devPartData.FTIRdata.devDetect"></el-input>
+                          </el-form-item>
+                          <el-form-item label="检测方法">
+                            <el-input v-model="devPartData.FTIRdata.methodDetect"></el-input>
+                          </el-form-item>
+                          <el-form-item label="上传文件">
+                            <el-upload
+                              action=""
+                              ref="uploadFTIR"
+                              :limit="10"
+                              :file-list="devPartData.FTIRdata.FTIRfileList"
+                              :on-change="handleChange"
+                              :on-preview="handlePreview"
+                              :before-upload="beforeFileUpload"> 
+                              <!-- :on-change="handleChange"  :auto-upload="false"-->
+                              <el-button 
+                                slot="trigger" 
+                                size="small" 
+                                type="primary"
+                                @click="handleIndex(index, 'FTIR')">
+                                点击选取
+                              </el-button>
+                            </el-upload>
+                          </el-form-item>
+                        </el-form>
+                      </div>
+                    </el-card>
+                  </el-col>
+
+                  <el-col :span="8">
+                    <el-card shadow="hover">
+                      <div slot="header">
+                        <span>RAMAN</span>
+                      </div>
+                      <div>
+                        <el-form 
+                          ref="RAMAN"
+                          :model="devPartData.RAMANdata"
+                          :label-position="labelPosition"
+                          label-width="100px">
+                          <el-form-item label="检测设备">
+                            <el-input v-model="devPartData.RAMANdata.devDetect"></el-input>
+                          </el-form-item>
+                          <el-form-item label="检测方法">
+                            <el-input v-model="devPartData.RAMANdata.methodDetect"></el-input>
+                          </el-form-item>
+                          <el-form-item label="上传文件">
+                            <!-- <el-button size="small" type="primary">点击上传</el-button> -->
+                            <el-upload
+                              action=""
+                              ref="uploadRAMAN"
+                              :limit="10"
+                              :file-list="devPartData.RAMANdata.RAMANfileList"
+                              :on-change="handleChange"
+                              :on-preview="handlePreview"
+                              :before-upload="beforeFileUpload"> 
+                              <!-- :on-change="handleChange"  :auto-upload="false"-->
+                              <el-button 
+                                slot="trigger" 
+                                size="small" 
+                                type="primary"
+                                @click="handleIndex(index, 'RAMAN')">
+                                点击选取
+                              </el-button>
+                            </el-upload>
+                          </el-form-item>
+                        </el-form>
+                      </div>
+                    </el-card>
+                  </el-col>
+
+                  <el-col :span="8">
+                    <el-card shadow="hover">
+                      <div slot="header">
+                        <span>XRF</span>
+                      </div>
+                      <div>
+                        <el-form 
+                          ref="XRF" 
+                          :model="devPartData.XRFdata"
+                          :label-position="labelPosition"
+                          label-width="100px">
+                          <el-form-item label="检测设备">
+                            <el-input v-model="devPartData.XRFdata.devDetect"></el-input>
+                          </el-form-item>
+                          <el-form-item label="检测方法">
+                            <el-input v-model="devPartData.XRFdata.methodDetect"></el-input>
+                          </el-form-item>
+                          <el-form-item label="上传文件">
+                            <el-button size="small" type="primary">点击上传</el-button>
+                          </el-form-item>
+                        </el-form>
+                      </div>
+                    </el-card>
+                  </el-col>
+
+                </el-row>
               </el-collapse-item>
             </el-collapse>
             <el-collapse > 
@@ -189,9 +317,9 @@
             </el-collapse>
           </div>
           <!-- 删除零件 -->
-          <div>
+          <!-- <div>
             <delete-button></delete-button>
-          </div>
+          </div> -->
         </el-card>
       </el-col>
     </el-row>
@@ -236,29 +364,45 @@ export default {
         Model: 'AM',
         Logo: 'AL',
         function: 'AF',
-        note: '1111'
+        note: '1111',
       },
-      devPartData: {
-        sname: 'A001',
-        Origin: 'AO',
-        Factory: 'AF',
-        Model: 'AM',
-        Logo: 'AL',
-        function: 'AF',
-        Color: '',
-        Material: '',
-        Shape: '',
-        thickness: '',
-        note: '1111'
-      },
+      devPartDataList: [
+        {
+          sname: 'A001',
+          Origin: 'AO',
+          Factory: 'AF',
+          Model: 'AM',
+          Logo: 'AL',
+          function: 'AF',
+          Color: '1',
+          Material: '1',
+          Shape: '1',
+          thickness: '1',
+          note: '1111',
+          key: Date.now(),
+          FTIRdata: {
+            devDetect: '',
+            methodDetect: '',
+            FTIRfileList: [],
+          },
+          RAMANdata: {
+            devDetect: '',
+            methodDetect: '',
+            RAMANfileList: [],
+          },
+          XRFdata: {
+            devDetect: '',
+            methodDetect: '',
+            XRFfileList: [],
+          },
+        },
+      ],
+      // FTIRfileList: [],
+      // uploadFTIRfile: {},
+      devPartIndex: 0,
+      devPartType: '',
       devRule: {
       },
-      FTIRData: {
-        devDetect: '',
-        methodDetect: ''
-      },
-      FTIRFile: [],
-      uploadFTIRFile: {}
     }
   },
   components: {
@@ -274,12 +418,103 @@ export default {
       'device',
       'token',
       'avatar',
-    ])
+    ]),
   },
+
+  mounted() {
+    this.uploadFTIRfile = new FormData()
+    console.log('+ + ExplosiveCreation mounted + + uploadFTIRfile is OK')
+  },
+
   methods: {
     handleAddDevPart() {
-
+      this.devPartDataList.push(
+        {
+          sname: 'A002',
+          Origin: 'AO',
+          Factory: 'AF',
+          Model: 'AM',
+          Logo: 'AL',
+          function: 'AF',
+          Color: '2',
+          Material: '2',
+          Shape: '2',
+          thickness: '2',
+          note: '222',
+          key: Date.now(),
+          FTIRdata: {
+            devDetect: '',
+            methodDetect: '',
+            FTIRfileList: [],
+            uploadFTIRfile: new FormData()
+          },
+          RAMANdata: {
+            devDetect: '',
+            methodDetect: '',
+            RAMANfileList: [],
+          },
+          XRFdata: {
+            devDetect: '',
+            methodDetect: '',
+            XRFfileList: [],
+          },
+        }
+      )
+      // console.log('- - before - - ref FTIR:', this.$refs.FTIR)
+      // console.log('- - before - - ref devPartForm:', this.$refs.devPartForm)
     },
+    handleDeleteDevPart(index) {
+      console.log('- - DeleteDevPart - - index: ', index)
+      this.devPartDataList.splice(index, 1)
+      console.log('- - DeleteDevPart - - FTIRfileList:', this.FTIRfileList)
+      for(let value of this.uploadFTIRfile.values()) {
+        console.log('- - DeleteDevPart- - uploadFTIRfile value:', value)
+      }
+    },
+    handleIndex(index, type) {
+      this.devPartIndex = index
+      this.devPartType = type
+    },
+
+    /* upload file */
+    handleChange(file, fileList) {
+      for(let fileItem of fileList) {
+        // console.log('- - Change - - fileItem:', fileItem)
+      }
+      // this.FTIRfileList = fileList
+      // console.log('- - Change - - FTIRfileList:', this.FTIRfileList)
+      // console.log('- - Change - - file:', file)
+    },
+    beforeFileUpload(file) {
+      console.log('- - before - - index:', this.devPartIndex)
+      console.log('- - before - - devPartType:', this.devPartType)
+      // console.log('- - before - - file typeof:', typeof(file))
+      // console.log('- - before - - file:', file)
+      if(this.devPartType === 'FTIR') {
+        this.devPartDataList[this.devPartIndex].FTIRdata.FTIRfileList.push(file)
+        console.log('- - before - - FTIRfileList:', this.devPartDataList[this.devPartIndex].FTIRdata.FTIRfileList)
+      } else {
+        this.devPartDataList[this.devPartIndex].RAMANdata.RAMANfileList.push(file)
+        console.log('- - before - - RAMANfileList:', this.devPartDataList[this.devPartIndex].RAMANdata.RAMANfileList)
+      }
+      
+      // this.uploadFTIRfile.append('txtURL', file)
+      // console.log('- - before- - uploadFTIRfile:', this.uploadFTIRfile)
+
+      // for(let key of this.uploadFTIRfile.keys()) {
+      //   console.log('- - before- - uploadFTIRfile key:', key)
+      // }
+      // // console.log('- - before- - uploadFTIRfile value len:', this.uploadFTIRfile.values())
+      // for(let value of this.uploadFTIRfile.values()) {
+      //   console.log('- - before- - uploadFTIRfile value:', value)
+      // }
+      return false
+    },
+    handlePreview(file) {
+      console.log('- - Preview - - file:', file.name, file)
+    },
+
+    /* submit */ 
     handleSubmit() {
       console.log('- - submit - - devData:', this.devData.sname)
     },
